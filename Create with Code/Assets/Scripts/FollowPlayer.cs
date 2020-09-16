64e2535496a533ad78e9db160a5e76d771d8b7f4
Sam Huntsman <10939244@my.uvu.edu>
2020-09-16 16:42:37 -0600

    Finished Prototype 1

--- Create with Code/Assets/Scripts/FollowPlayer.cs
+++ Create with Code/Assets/Scripts/FollowPlayer.cs
@@ -6,7 +6,7 @@ public class FollowPlayer : MonoBehaviour
 {
     // Sets the target for the camera to follow
     public GameObject player;
-    public Vector3 offset = new Vector3(0, 5, -10);
+    private Vector3 offset = new Vector3(0, 5, -10);
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +17,7 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Makes the main camera follow the player position
         transform.position = player.transform.position +  offset;
     }
 }
--- Create with Code/Assets/Scripts/PlayerController.cs
+++ Create with Code/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public int speed = 20;
-    public float turnSpeed = 10;
-    public float hInput;
-    public float fInput;
+    private int speed = 20;
+    private float turnSpeed = 100;
+    private float hInput;
+    private float fInput;
 
     // Start is called before the first frame update
     void Start()
@@ -21,8 +21,9 @@ public class PlayerController : MonoBehaviour
         hInput = Input.GetAxis("Horizontal");
         fInput = Input.GetAxis("Vertical");
 
-        // Move the vehicle forward
+        // Move the vehicle forward based on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * fInput * speed);
+        // Rotates the vehicle left and right based on horizontal input
         transform.Rotate(Vector3.up * Time.deltaTime * hInput * turnSpeed);
     }
 }
