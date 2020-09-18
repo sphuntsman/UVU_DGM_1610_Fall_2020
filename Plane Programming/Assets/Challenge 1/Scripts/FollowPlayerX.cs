b7820217b87f10adf8cff3b4a27e80625aa4ee66
Sam Huntsman <10939244@my.uvu.edu>
2020-09-17 18:12:01 -0600

    Finished Challenge 1: Plane Programming

---
+++ Plane Programming/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    // Sets the target for the camera to follow
+    public GameObject plane;
+    private Vector3 offset = new Vector3(30, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Makes the main camera follow the player position
+        transform.position = plane.transform.position + offset;
+    }
+}
---
+++ Plane Programming/Assets/Challenge 1/Scripts/PropellerMover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerMover : MonoBehaviour
+{
+    // Rotates the plane's propeller
+    private float RotationSpeed = 500;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotates the propeller on the plane at a constant rate
+        transform.Rotate(Vector3.forward * RotationSpeed);
+    }
+}
