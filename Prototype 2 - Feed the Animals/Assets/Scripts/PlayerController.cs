79bcad8ccdff9edd9b18fa037fb2ff99780ba626
Sam Huntsman <10939244@my.uvu.edu>
2020-09-23 17:03:39 -0600

    Added Prototype 2 Assets. Coded Player movement and constraints.

--- Prototype 2 - Feed the Animals/Assets/Scripts/PlayerController.cs
+++ Prototype 2 - Feed the Animals/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float speed = 10.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,5 +17,17 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if(transform.position.x < -10)
+        {
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        }
+        
+        if(transform.position.x > 10)
+        {
+            transform.position = new Vector3(10, transform.position.y, transform.position.z);
+        }
     }
 }
