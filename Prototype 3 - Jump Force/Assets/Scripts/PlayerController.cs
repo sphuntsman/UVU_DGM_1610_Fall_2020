649866eaa299f7d10c9ba5d7928c42094660896c
Sam Huntsman <10939244@my.uvu.edu>
2020-10-21 16:56:37 -0600

    Completed jump and fall animations. Prevented post death jump.

--- Prototype 3 - Jump Force/Assets/Scripts/PlayerController.cs
+++ Prototype 3 - Jump Force/Assets/Scripts/PlayerController.cs
@@ -11,20 +11,24 @@ public class PlayerController : MonoBehaviour
     private bool isGrounded = true;
     public bool gameOver = false;
 
+    private Animator playerAnim;
+
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityMod;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded == true && !gameOver)
         {
             isGrounded = false;
             playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
@@ -39,6 +43,8 @@ public class PlayerController : MonoBehaviour
         {
             gameOver = true;
             Debug.Log("GAME OVER MAN! -- GAME OVER!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
