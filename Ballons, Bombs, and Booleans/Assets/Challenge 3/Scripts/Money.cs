b24d298650c04a24e8fe7124e92113b3860faf61
Sam Huntsman <10939244@my.uvu.edu>
2020-11-02 13:34:18 -0700

    Finished Challenge 3: Balloons, Bombs, and Booleans.

---
+++ Ballons, Bombs, and Booleans/Assets/Challenge 3/Scripts/Money.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Money : MonoBehaviour
+{
+    public float speed = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+    }
+}
