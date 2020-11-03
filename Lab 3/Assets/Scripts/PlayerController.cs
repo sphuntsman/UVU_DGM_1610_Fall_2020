3913bb25dda03d4095ce781240057e1fdc9da0be
Sam Huntsman <10939244@my.uvu.edu>
2020-11-03 12:55:15 -0700

    Completed Lab 3.

---
+++ Lab 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Forgot to declare the variable after I saved the code
+    public float vInput;
+    // Player's movement speed
+    private float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Collecting the input
+        vInput = Input.GetAxis("Vertical");
+        // Move up or down based on the arrow keys
+        transform.Translate(Vector3.forward * vInput * Time.deltaTime * speed);
+        // Boundaries for the player
+        if(transform.position.z < -4)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -4);
+        }
+
+        if(transform.position.z > 4)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 4);
+        }
+    }
+}
