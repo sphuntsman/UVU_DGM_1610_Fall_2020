be0e683f4a371974e6485aff5915aec5d4f6b09c
Sam Huntsman <10939244@my.uvu.edu>
2020-11-04 17:19:07 -0700

    Completed Lesson 4.1 - Watch Where You're Going!

---
+++ Prototype 4 - Sumo Battle/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+
+    public float rotationSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get horizontal input for camera rotation around the z-axis
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+    }
+}
