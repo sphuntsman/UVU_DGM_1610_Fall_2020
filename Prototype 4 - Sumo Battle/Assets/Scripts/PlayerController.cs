3d3b74e8fc6557c9a97735acfeef60ecdb28b521
Sam Huntsman <10939244@my.uvu.edu>
2020-11-11 17:24:26 -0700

    Completed Lesson 4.3

--- Prototype 4 - Sumo Battle/Assets/Scripts/PlayerController.cs
+++ Prototype 4 - Sumo Battle/Assets/Scripts/PlayerController.cs
@@ -6,8 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     public float speed;
+    public bool hasPowerUp;
     
     public GameObject focalPoint;
+
+    private float powerUpStr = 15.0f;
+
+    public GameObject powerUpIndicator;
 
     // Start is called before the first frame update
     void Start()
@@ -21,5 +26,41 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput * Time.deltaTime);
+
+        powerUpIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(collider.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            Debug.Log("PowerUp = " + hasPowerUp);
+            Destroy(collider.gameObject);
+            StartCoroutine(PowerUpCountDown());
+            powerUpIndicator.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            Debug.Log("Player collided with " + collision.gameObject + " with powerup set to " + hasPowerUp);
+
+            enemyRigidbody.AddForce(awayFromPlayer * powerUpStr, ForceMode.Impulse);
+
+        }
+    }
+
+    IEnumerator PowerUpCountDown()
+    {
+        yield return new WaitForSeconds(7); hasPowerUp = false;
+        powerUpIndicator.gameObject.SetActive(false);
     }
 }
