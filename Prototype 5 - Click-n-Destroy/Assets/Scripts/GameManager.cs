adef9dfc492f34ea1a68c5bc049ae6ae3045ff2f
Sam Huntsman <10939244@my.uvu.edu>
2020-12-07 17:06:30 -0700

    Completed up to Lesson 5.3

---
+++ Prototype 5 - Click-n-Destroy/Assets/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+
+    private float spawnRate = 1.0f;
+
+    private int score;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverTxt;
+
+    public Button restartButton;
+
+    public bool isGameActive;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        gameOverTxt.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(false);
+        isGameActive = true;
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("GameOver");
+        gameOverTxt.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        Debug.Log("Spawning");
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+}
